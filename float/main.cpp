4e2e4b67e80a747635d51b130026e00f8c9f513a
eencae <3027785+eencae@users.noreply.github.com>
2018-09-18 15:47:17 +0100

    Updates to testing example code

---
+++ float/main.cpp
@@ -0,0 +1,56 @@
+#include <cmath>  // for std::abs
+#include <iostream>
+#include <limits>
+
+// function we have written
+double sqr(double a);
+// function to test function
+bool test_sqr(double a, double expected);
+// function to run the tests
+int run_sqr_tests();
+// tests whether two double are almost equal
+bool almost_equal(double a, double b);
+
+int main() {
+// test the function
+#ifdef DEBUG
+  run_sqr_tests();
+#endif
+}
+
+// function we have implemented
+double sqr(double a) { return a * a; }
+
+// function to test sqr
+bool test_sqr(double a, double expected) {
+  std::cout << "sqr(" << a << ") : ";
+  double val = sqr(a);  // calc value and compare to expected
+  if (almost_equal(val, expected)) {
+    std::cout << "passed\n";
+    return true;
+  } else {
+    std::cout << "FAILED! " << val << " (expecting " << expected << ").\n";
+    return false;
+  }
+}
+
+// function to run the tests
+int run_sqr_tests() {
+  std::cout << "\nTesting sqr()...\n" << std::endl;
+  // initialise counter for number of tests passed
+  int passed = 0;
+  // do various tests
+  if (test_sqr(-1.123456789, 1.2621551568)) passed++;
+  if (test_sqr(1.123456789e25, 1.2621552e+50)) passed++;
+  if (test_sqr(1.123456789e-5, 1.2621552e-10)) passed++;
+  if (test_sqr(3.0, 9)) passed++;
+  if (test_sqr(7.678, 58.951684)) passed++;
+  std::cout << "\nsqr() passed " << passed << " tests.\n";
+  return passed;
+}
+// checks whether 2 double are almost equal
+bool almost_equal(double a, double b) {
+  // == may return true, but also check whether the relative difference
+  // between the numbers is less than a small threshold
+  return a == b || (std::abs(a - b) / std::max(a, b) <= 1e-6);
+}
--- simple/main.cpp
+++ simple/main.cpp
@@ -3,55 +3,44 @@
 // function we have written
 int sum(int a, int b);
 // function to test function
-int test_sum();
+bool test_sum(int a, int b, int expected);
+// function to run the tests
+int run_sum_tests();
 
 int main() {
   // test the function
-  test_sum();
+  run_sum_tests();
 
-  std::cout << "\n...continue with main()..." << std::endl;
+  std::cout << "\n...continue with main()...\n";
 }
+
 // function we have implemented
-int sum(int a, int b) { return a + b; }
-// testing function
-int test_sum() {
-  std::cout << "\nTesting sum()..." << std::endl;
+int sum(int a, int b) { return a - b; }
+
+// function to test sum
+bool test_sum(int a, int b, int expected) {
+  std::cout << "sum(" << a << "," << b << ") : ";
+  int val = sum(a, b);  // calc value and compare to expected
+  if (val == expected) {
+    std::cout << "passed\n";
+    return true;
+  } else {
+    std::cout << "FAILED! " << val << " (expecting " << expected << ").\n";
+    return false;
+  }
+}
+
+// function to run the tests
+int run_sum_tests() {
+  std::cout << "\nTesting sum()...\n" << std::endl;
   // initialise counter for number of tests passed
-  int tests_passed = 0;
-  // do several tests, checking for correct return value
-  // and print appropriate message
-  if (sum(0, 0) == 0) {
-    std::cout << "passed sum(0, 0) == 0" << std::endl;
-    tests_passed++;
-  } else {
-    std::cout << "failed sum(0, 0) == 0" << std::endl;
-  }
-  if (sum(-1, 1) == 0) {
-    std::cout << "passed sum(-1, 1) == 0" << std::endl;
-    tests_passed++;
-  } else {
-    std::cout << "failed sum(-1, 1) == 0" << std::endl;
-  }
-  if (sum(2, 3) == 5) {
-    std::cout << "passed sum(2, 3) == 5" << std::endl;
-    tests_passed++;
-  } else {
-    std::cout << "failed sum(2, 3) == 5" << std::endl;
-  }
-  if (sum(826109, 78657567) == 79483676) {
-    std::cout << "passed sum(826109, 78657567) == 79483676" << std::endl;
-    tests_passed++;
-  } else {
-    std::cout << "failed sum(826109, 78657567) == 79483676" << std::endl;
-  }
-  if (sum(-99, -1) == -100) {
-    std::cout << "passed sum(-99, -1) == -100" << std::endl;
-    tests_passed++;
-  } else {
-    std::cout << "failed sum(-99, -1) == -100" << std::endl;
-  }
-  // print summary message and return number of tests passed
-  // may not want/need to use the return value in main
-  std::cout << "sum() passed " << tests_passed << "/5 tests." << std::endl;
-  return tests_passed;
+  int passed = 0;
+  // do various tests
+  if (test_sum(0, 0, 0)) passed++;
+  if (test_sum(-1, 1, 0)) passed++;
+  if (test_sum(2, 3, 5)) passed++;
+  if (test_sum(826109, 78657567, 79483676)) passed++;
+  if (test_sum(-99, -1, -100)) passed++;
+  std::cout << "\nsum() passed " << passed << " tests.\n";
+  return passed;
 }
